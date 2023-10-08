21eac3ab9fe3490d9fe026efbec79957b8b8d83d
Xiangrui Sun <sunxiangruisjy@163.com>
2023-10-08 19:15:43 -0400

    progress

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -2,16 +2,21 @@
 
 __author__ = "730525294"
 
-def all(list_int: list, int_find: int) -> bool:
+
+def all(list_int: list[int], int_find: int) -> bool:
     """To detect whether all the ints in the list are the same as the given int."""
     idx: int = 0
+    if list_int == []:
+        return False
+    
     while idx < len(list_int):
         if list_int[idx] != int_find:
             return False
         idx += 1
     return True
 
-def max(list_int: list) -> int:
+
+def max(list_int: list[int]) -> int:
     """To return the largest int in the list."""
     if len(list_int) == 0:
         raise ValueError("max() arg is an empty List")
@@ -25,10 +30,10 @@ def max(list_int: list) -> int:
         idx += 1
     return int_max
 
-def is_equal(list_1: list, list_2: list) -> bool:
+
+def is_equal(list_1: list[int], list_2: list) -> bool:
     """To identify whether two lists are identical."""
     idx: int = 0
-    indicator: bool = False
     if len(list_1) != len(list_2):
         return False
     
@@ -37,9 +42,4 @@ def is_equal(list_1: list, list_2: list) -> bool:
             idx += 1
         else:
             return False
-    return True
-
-
-
-
-        
+    return True       
